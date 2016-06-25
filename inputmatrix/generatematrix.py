ffabc83ce1a57c4996ecd275d97687dcea5a3c61
Pawan Mani Teja <maniteja@Pawans-MacBook-Pro.local>
2016-06-25 12:30:17 +0530

    jd new def

--- inputmatrix/generatematrix.py
+++ inputmatrix/generatematrix.py
@@ -182,6 +182,51 @@ def extractfeaturescore_companies(config, txtfile, row):
     row['cmp_score'] = cmpscore/cmplen
 	return row
 
+def extractfeaturescore_jd_new(config, txtfile, jd, row):
+	File = open(config['txts_folder']+txtfile,'r')
+	txt = File.read()
+	File.close()
+	# print(config['txts_folder']+txtfile)
+	punctuation = list(string.punctuation)
+	stop_words = set(stopwords.words('english') + punctuation)
+	sents = []
+	word_tokens = []
+	try:
+		sents = sent_tokenize(txt.decode('utf-8'))
+	except Exception as e:
+		print("\n\n"+txtfile+" sents ****** and error: "+str(e))
+	jddict ={}
+	try:
+		word_tokens = word_tokenize(jd["c_feature"])
+	except Exception as e:
+		print("\n\n"+txtfile+" jd c ++++++ and error: "+str(e))
+	c_feature = [w for w in word_tokens if not w in stop_words]
+	jddict['c_feature'] = c_feature
+	# print([w for w in jd])
+	try:
+		word_tokens = word_tokenize(jd["Technical"])
+	except Exception as e:
+		print("\n\n"+txtfile+" jd t ++++++ and error: "+str(e))
+	
+	Technical = [w for w in word_tokens if not w in stop_words]
+	jddict['Technical'] = Technical
+	try:
+		word_tokens = word_tokenize(jd["Role"])
+	except Exception as e:
+		print("\n\n"+txtfile+" jd r ++++++ and error: "+str(e))
+	
+	Role = [w for w in word_tokens if not w in stop_words]
+	jddict['Role'] = Role
+	# print(jddict)
+	c_count = 0
+	r_count = 0
+	t_count = 0
+
+	row['c_count'] = c_count
+	row['r_count'] = r_count
+	row['t_count'] = t_count
+	return row
+
 def genMatrix(filenames):
 	global matrix
 	for file in filenames:
