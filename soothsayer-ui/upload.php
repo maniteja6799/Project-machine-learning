2bd5279017fea07beddf0374cf95e68f9dc481c7
shivam <shivam.kumar110@gmail.com>
2016-06-29 02:19:50 +0530

    Last Night Commit

--- soothsayer-ui/upload.php
+++ soothsayer-ui/upload.php
@@ -1,33 +1,17 @@
 <?php
+$ds          = DIRECTORY_SEPARATOR;  //1
 
-// Check for errors
-if($_FILES['file_upload']['error'] > 0){
-    die('An error ocurred when uploading.');
+$storeFolder = 'upload';   //2
+
+if (!empty($_FILES)) {
+
+    $tempFile = $_FILES['file']['tmp_name'];          //3
+
+    $targetPath = dirname( __FILE__ ) . $ds. $storeFolder . $ds;  //4
+    $textFileName = "sample.txt";
+    $targetFile =  $targetPath.$textFileName;  //5
+
+    move_uploaded_file($tempFile,$targetFile); //6
+
 }
-
-// if(!getimagesize($_FILES['file_upload']['tmp_name'])){
-//     die('Please ensure you are uploading an image.');
-// }
-
-// Check filetype
-// if($_FILES['file_upload']['type'] != 'image/png'){
-//     die('Unsupported filetype uploaded.');
-// }
-
-// Check filesize
-// if($_FILES['file_upload']['size'] > 500000){
-//     die('File uploaded exceeds maximum upload size.');
-// }
-
-// Check if the file exists
-if(file_exists('upload/' . $_FILES['file_upload']['name'])){
-    die('File with that name already exists.');
-}
-
-// Upload file
-if(!move_uploaded_file($_FILES['file_upload']['tmp_name'], 'upload/' . $_FILES['file_upload']['name'])){
-    die('Error uploading file - check destination is writeable.');
-}
-
-die('File uploaded successfully.');
 ?>
